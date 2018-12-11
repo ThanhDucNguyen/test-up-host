45dc7308dfe51b4775f4f276b399d0aa79d02c3e
Nguyen Thanh Duc <ntduc2812@gmail.com>
2018-12-11 23:39:52 +0700

    sad

--- src/main/java/com/hoc1/demo/controller/ContactController.java
+++ src/main/java/com/hoc1/demo/controller/ContactController.java
@@ -1,7 +1,6 @@
 package com.hoc1.demo.controller;
 
-import com.hoc1.demo.Service.ContactService;
-import com.hoc1.demo.domain.Contact;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,56 +16,12 @@ import javax.validation.Valid;
 @Controller
 public class ContactController {
     @Autowired
-    private ContactService contactService;
+
 
     @GetMapping("/")
-    public String hl(Model model) {
-        model.addAttribute("contacts", contactService.findAll());
+    public String hl() {
         return "index";
     }
 
-    @GetMapping("/contact")
-    public String index(Model model) {
-        model.addAttribute("contacts", contactService.findAll());
-        return "list";
-    }
-    @GetMapping("/contact/{id}/delete")
-    public String delete(@PathVariable int id) {
-        contactService.delete(id);
-        return "redirect:/contact";
-    }
-    @GetMapping("/contact/{id}/edit")
-    public String edit(@PathVariable int id, Model model) {
-        model.addAttribute("contact", contactService.findOne(id));
-        return "form";
-    }
 
-    @GetMapping("/contact/create")
-    public String create(Model model) {
-        model.addAttribute("contact", new Contact());
-        return "form";
-    }
-    @PostMapping("/contact/save")
-    public String save(@Valid Contact contact, BindingResult result, RedirectAttributes redirect) {
-        if (result.hasErrors()) {
-            return "form";
-        }
-        contactService.save(contact);
-        redirect.addFlashAttribute("success", "Saved contact successfully!");
-        return "redirect:/contact";
-    }
-    @GetMapping("/contact/search")
-    public String search(@RequestParam("q") String q, Model model) {
-        if (q.equals("")) {
-            return "redirect:/contact";
-        }
-
-        model.addAttribute("contacts", contactService.search(q));
-        return "list";
-    }
-    @GetMapping("/bus/{id}/seat")
-    public String list(@PathVariable int id) {
-        contactService.pro(id);
-        return "form";
-    }
 }
